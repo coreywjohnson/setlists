57327b43f19bf12c55927d323c478bb7cdd83e5f
Corey Johnson <corey.johnson@kwickie.com>
2016-05-17 16:42:30 +1000

    Introduced scopes, allowing setlist service to become a singleton

--- app/src/main/java/com/coreywjohnson/setlists/components/AppComponent.java
+++ app/src/main/java/com/coreywjohnson/setlists/components/AppComponent.java
@@ -4,12 +4,15 @@ import com.coreywjohnson.setlists.interactors.SearchArtistInteractor;
 import com.coreywjohnson.setlists.modules.AppModule;
 import com.coreywjohnson.setlists.modules.InteractorsModule;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
 /**
  * Created by coreyjohnson on 4/05/16.
  */
 
+@Singleton
 @Component(
         modules = {AppModule.class, InteractorsModule.class}
 )
