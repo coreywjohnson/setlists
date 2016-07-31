97e5b6605d48bcdb33f14f651c772bb1234d0174
coreyjohnson <corey.johnson@outlook.com>
2016-07-31 14:31:43 +1000

    Added user-agent and authorization headers to all requests

--- app/src/main/java/com/coreywjohnson/setlists/modules/InteractorsModule.java
+++ app/src/main/java/com/coreywjohnson/setlists/modules/InteractorsModule.java
@@ -2,10 +2,16 @@ package com.coreywjohnson.setlists.modules;
 
 import com.coreywjohnson.setlists.data.SetlistService;
 
+import java.io.IOException;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
@@ -18,9 +24,25 @@ public class InteractorsModule {
     @Singleton
     @Provides
     public Retrofit retrofit() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request original = chain.request();
+                        Request request = original.newBuilder()
+                                .header("User-Agent", "Setlists for Android")
+                                .header("Authorization", "569099de-9e96-4429-9609-a063c5198242")
+                                .method(original.method(), original.body())
+                                .build();
+                        return chain.proceed(request);
+                    }
+                })
+                .build();
+
         return new Retrofit.Builder()
                 .baseUrl(SetlistService.BASE_API_URL)
                 .addConverterFactory(SimpleXmlConverterFactory.createNonStrict())
+                .client(client)
                 .build();
     }
 
